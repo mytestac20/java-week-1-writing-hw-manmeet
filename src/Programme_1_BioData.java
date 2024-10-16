d1fdbdb9b234eb620f0e5312da1522fd0f171dfe
mytestac20 <mytestac20@gmail.com>
2024-10-16 23:23:32 +0100

    My homework week 6

---
+++ src/Programme_1_BioData.java
@@ -0,0 +1,23 @@
+/**
+ * 1.Write a programme that prints your Bio Data.
+ */
+public class Programme_1_BioData {
+    public static void main (String[] args){
+        System.out.println("            BIO DATA                   ");
+        System.out.println(" Name           : Manmeet Kaur");
+        System.out.println(" Mobile         : 07501138386");
+        System.out.println(" Email          : Mytestac20@gmail.com");
+        System.out.println(" Gender         : Female");
+        System.out.println(" Date of Birth  : 20.02.2000");
+        System.out.println(" Marital Status : Married");
+        System.out.println(" Language Known : English,Hindi");
+        System.out.println(" Qualification  : Software tester");
+        System.out.println(" Experience     : Agile Tester with 5 years of Experience");
+        System.out.println(" Address        : 123 Abc Street, London sg1 3 jx");
+        System.out.println(" Place          : London");
+        System.out.println("Date            : 02.01.2022");
+        System.out.println("Signature       : Mkaur");
+    }
+
+
+}
---
+++ src/Programme_7_SumOfTwoNumber.java
@@ -0,0 +1,17 @@
+
+/**
+ * 7. Write a Java program to print the sum of two numbers.
+ * Test Data: 74 + 36
+ * Expected Output: 110
+ */
+public class Programme_7_SumOfTwoNumber {
+
+    public static void main(String [] args){
+
+    //variables
+    int a = 74;
+    int b = 36;
+    int sum = a + b ;
+        System.out.println("The sum of " + a +" and " + b + " is " + sum );
+    }
+}
